556b16c2d8a5a4bf34fc5219f05513f8ffcc9728
farhan <syd.farru@gmail.com>
2023-02-04 05:55:51 +0530

    added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Add3 Front-end Take Home
+
+This is a front-end project for the role of front-end engineer at Add3. In this Readme I will try my best to explain how to use this web app, so keep reading.
+
+## Step 1: Connecting/Disconnecting MetaMask Wallet
+
+The green button on top right corner that reads 'Connect to MetaMask' connects the web app to your MetaMask account. You will be asked to choose an account of your liking from a dropdown after clicking this button. Once connected, it will turn blue and display your account address.
+
+In order to disconnect MetaMask, click the same button again OR refresh the page.
+
+## Step 2: Transferring Goerli tokens in MetaMask account in order to execute smart contract functions
+
+First, create an Alchemy account if you don't already have one. Then, go to https://goerlifaucet.com/ and enter your MetaMask account address in order to receive free goerli tokens.
+
+## Step 3: Enter RPC URL in MetaMask
+
+Create a new Network on MetaMask and add the RPC URL from your Alchemy profile. This will connect your account to the Goerli Network.
+
+## Step 3: Finally, you are ready to Mint Tokens!
+
+In the Dashboard, you will find an input field. Try entering your account address after copying it from MetaMask and notice that the Account Balance displayed on the left changes.
--- src/components/widgets/WidgetsDropdown.js
+++ src/components/widgets/WidgetsDropdown.js
@@ -4,167 +4,9 @@ import { useWalletContext } from '../../WalletContext'
 
 import { CRow, CCol, CWidgetStatsA, CInputGroup, CFormInput, CButton, CAlert } from '@coreui/react'
 
-// Smart Contract ABI.
+// smart contract ABI.
 
-const contractABI = [
-  { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: 'address', name: 'owner', type: 'address' },
-      { indexed: true, internalType: 'address', name: 'spender', type: 'address' },
-      { indexed: false, internalType: 'uint256', name: 'value', type: 'uint256' },
-    ],
-    name: 'Approval',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: 'address', name: 'previousOwner', type: 'address' },
-      { indexed: true, internalType: 'address', name: 'newOwner', type: 'address' },
-    ],
-    name: 'OwnershipTransferred',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: 'address', name: 'from', type: 'address' },
-      { indexed: true, internalType: 'address', name: 'to', type: 'address' },
-      { indexed: false, internalType: 'uint256', name: 'value', type: 'uint256' },
-    ],
-    name: 'Transfer',
-    type: 'event',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'owner', type: 'address' },
-      { internalType: 'address', name: 'spender', type: 'address' },
-    ],
-    name: 'allowance',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'spender', type: 'address' },
-      { internalType: 'uint256', name: 'amount', type: 'uint256' },
-    ],
-    name: 'approve',
-    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
-    name: 'balanceOf',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'decimals',
-    outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'spender', type: 'address' },
-      { internalType: 'uint256', name: 'subtractedValue', type: 'uint256' },
-    ],
-    name: 'decreaseAllowance',
-    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'spender', type: 'address' },
-      { internalType: 'uint256', name: 'addedValue', type: 'uint256' },
-    ],
-    name: 'increaseAllowance',
-    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'to', type: 'address' },
-      { internalType: 'uint256', name: 'amount', type: 'uint256' },
-    ],
-    name: 'mint',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'name',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'owner',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'renounceOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'symbol',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'totalSupply',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'to', type: 'address' },
-      { internalType: 'uint256', name: 'amount', type: 'uint256' },
-    ],
-    name: 'transfer',
-    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'from', type: 'address' },
-      { internalType: 'address', name: 'to', type: 'address' },
-      { internalType: 'uint256', name: 'amount', type: 'uint256' },
-    ],
-    name: 'transferFrom',
-    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'address', name: 'newOwner', type: 'address' }],
-    name: 'transferOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-]
+const contractABI = require('../../contract/contractABI.json')
 
 const WidgetsDropdown = () => {
   // function to Mint tokens using smart contract.
@@ -175,13 +17,18 @@ const WidgetsDropdown = () => {
       message: `Minting 100 ${tokenSymbol} tokens to address ${userAddress}... please wait...`,
       color: 'info',
     })
-    await contract.mint(userAddress, 100).then(() => {
-      setAlertState({
-        state: 'active',
-        message: `Minting complete! 100 ${tokenName} tokens have been minted to address ${userAddress} !`,
-        color: 'success',
+    await contract
+      .mint(userAddress, 100)
+      .then(() => {
+        setAlertState({
+          state: 'active',
+          message: `Minting complete! 100 ${tokenName} tokens have been minted to address ${userAddress} !`,
+          color: 'success',
+        })
       })
-    })
+      .then(() => {
+        getTokenBalance()
+      })
   }
   // function to get the Token related data.
 
@@ -226,11 +73,10 @@ const WidgetsDropdown = () => {
     //   (network = 'goerly'),
     //   'Mjit0Jq89qciTYJWQZLTvs9MCxyf_Mxd',
     // )
-    const signer = new ethers.Wallet(
-      '4337f69247d759dd1f2247d57864fdbf6bb73d056a2301f1f8d37a2591b21dae',
-      provider,
-    )
-    const smartContractAddress = '0x927DFb9e957526e4D40448d6D05A39ea39a2ee6B'
+    console.log(typeof process.env.REACT_APP_WALLET_ADDRESS)
+    const signer = new ethers.Wallet(process.env.REACT_APP_WALLET_ADDRESS, provider)
+    const smartContractAddress = process.env.REACT_APP_SMART_CONTRACT_ADDRESS
+    console.log(typeof smartContractAddress)
     setContractAddress(new ethers.Contract(smartContractAddress, contractABI, signer))
     console.log(contractAddress)
 
@@ -246,7 +92,6 @@ const WidgetsDropdown = () => {
 
   // using states:
 
-  // const [accountAddress, setAccountAddress] = useState('')
   const [accountBalance, setAccountBalance] = useState('Nil')
   const [tokenName, setTokenName] = useState('Nil')
   const [tokenSymbol, setTokenSymbol] = useState('')
@@ -268,23 +113,7 @@ const WidgetsDropdown = () => {
           <CWidgetStatsA
             className="mb-4"
             color="primary"
-            value={
-              walletConnected ? (
-                <>
-                  Balance: {accountBalance}
-                  {/* <span className="fs-6 fw-normal">
-                  (-12.4% <CIcon icon={cilArrowBottom} />)
-                </span> */}
-                </>
-              ) : (
-                <>
-                  Balance: Nil
-                  {/* <span className="fs-6 fw-normal">
-                  (-12.4% <CIcon icon={cilArrowBottom} />)
-                </span> */}
-                </>
-              )
-            }
+            value={walletConnected ? <>Balance: {accountBalance}</> : <>Balance: Nil</>}
             // title={'Token: ' + tokenName + (tokenSymbol !== '' ? '(' + tokenSymbol + ')' : '')}
             title={walletConnected ? `Token: ${tokenName} (${tokenSymbol})` : 'Token: Nil'}
             style={{ paddingBottom: '15px' }}
